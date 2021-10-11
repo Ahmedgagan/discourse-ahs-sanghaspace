bf8c194e788162b57dcf35af2e9650169008d4ef
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-10-11 16:17:47 +0530

    DEV: makes logic flexible

--- plugin.rb
+++ plugin.rb
@@ -1,28 +1,24 @@
 # frozen_string_literal: true
 
-# name: discourse-allow=fields
-# about: Allows field for topic tracking state
+# name: discourse-ahs-sanghaspace
 # version: 0.1
 # author: Ahmed Gagan
-# url: https://github.com/Ahmedgagan/discourse-allow-fields
+# url: https://github.com/Ahmedgagan/discourse-ahs-sanghaspace
 
-enabled_site_setting :discourse_allow_fields_enabled
+enabled_site_setting :discourse_ahs_sanghaspace_enabled
 
 after_initialize do
-  SiteSetting.discourse_allow_fields_name.split('|').each do |field|
-    add_to_serializer(:topic_tracking_state, field) do
+  SiteSetting.discourse_ahs_sanghaspace_topic_fields_name.split('|').each do |field|
+    value = field.to_sym
+    add_to_serializer(:topic_tracking_state, value) do
       topic = Topic.find(object.topic_id)
-      topic[field]
-    end
-  end
+      return topic[value] if topic[value]
 
-  add_to_serializer(:topic_tracking_state, :solved) do
-    topic = Topic.find(object.topic_id)
-    !!topic.custom_fields["accepted_answer_post_id"]
+      topic.custom_fields[value]
+    end
   end
 
   add_to_serializer(:current_user, :bookmark_count) do
     scope.user.bookmarks.filter { |bookmark| bookmark.post }.count
   end
-
 end
